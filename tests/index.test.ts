e2d399c7ff3c4e60f142f2f66123f9ac643b9b93
Michael James <micrjames921@gmail.com>
2024-03-12 18:59:32 -0700

    added tests directory and template file for a test suite

---
+++ tests/index.test.ts
@@ -0,0 +1,26 @@
+const {} = require("");
+
+describe("", () => {
+   beforeAll(() => {
+   });
+   describe("", () => {
+	  test.todo("", () => {
+	  });
+	  test.todo("", () => {
+	  });
+	  test.todo("", () => {
+	  });
+	  test.todo("", () => {
+	  });
+   });
+   describe("", () => {
+	  test.todo("", () => {
+	  });
+	  test.todo("", () => {
+	  });
+	  test.todo("", () => {
+	  });
+	  test.todo("", () => {
+	  });
+   });
+});
